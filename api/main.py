7a61f49415e0e21b365bf4afe7b2d6be502df11a
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-12-09 21:21:09 -0800

    dashboard + clean API

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ def health_check():
 @app.get("/scan/{package_name}")
 def scan_package(package_name: str):
     # 1. Fetch Raw Data (BigQuery)
-    print(f"***** Fetching {package_name}...")
+    print(f"Fetching {package_name}...")
     raw_data = fetch_package_metadata(package_name)
 
     if not raw_data:
@@ -25,11 +25,12 @@ def scan_package(package_name: str):
         )
 
     # 2. Transform Features (Pipeline)
-    # TODO: it's broken...lol
     print("Running feature engineering...")
     try:
         # Pass the dict directly to our new adapter
         features_json = transform_single_package(raw_data)
+
+        # Make the DF for model.predict() to work
         features = pd.DataFrame.from_dict([features_json])
     except Exception as e:
         print(f"Pipeline Error: {e}")
@@ -37,22 +38,20 @@ def scan_package(package_name: str):
             status_code=500, detail=f"Feature engineering failed: {str(e)}"
         )
 
-    # TODO 3. Predict
+    # 3. Predict
+    # Load the model
     model = joblib.load("models/ensemble.joblib")
 
+    # Try and predict, and throw if it fails
     try:
         prediction = model.predict_proba(features)[0][1]
     except Exception as e:
-        print(f"******* Prediction Error: {e}")
+        print(f"Prediction Error: {e}")
         raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
-
-    # For now, let's just return the features to prove the pipeline works
-    # TODO: the return should include prediction, AND features, AND raw_data
 
     return {
         "package": package_name,
-        "features_generated": len(features_json),
-        "sample_feature": features_json.get("n_name_len"),  # Just to check logic
-        "full_features": features_json,
+        "raw_data": raw_data,
+        "features": features_json,
         "prediction": prediction,
     }
